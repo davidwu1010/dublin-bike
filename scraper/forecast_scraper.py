22b17c168c0226f584b79c504365e065924d97da
David Wu <wudawei01@gmail.com>
2020-02-15 21:29:32 +0000

    Refactor forecast_scraper.py

--- scraper/forecast_scraper.py
+++ scraper/forecast_scraper.py
@@ -16,18 +16,17 @@ class Forecast(Base):
 
 def scrape():
     host = 'database-1.cmv75f0i1uzy.eu-west-1.rds.amazonaws.com'
-    engine = create_engine(f"mysql+pymysql://dev:qwerty@{host}/development")
+    engine = create_engine(f'mysql+pymysql://dev:qwerty@{host}/development')
     Base.metadata.create_all(engine)  # Create table
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    key = '9045a4958d8f45e1a54f6607ff2ed1d2'
-    city = 'Dublin'
-    country = 'IE'
-    hours = 6
-    api = f'https://api.weatherbit.io/v2.0/forecast/hourly?city={city}&country={country}&hours={hours}&key={key}'
+    parameters = {'city': 'Dublin', 'country': 'IE', 'hours': '6',
+                  'key': '9045a4958d8f45e1a54f6607ff2ed1d2'}
+    api = 'https://api.weatherbit.io/v2.0/forecast/hourly'
 
-    response = requests.get(api)
+    response = requests.get(api, params=parameters)
+    response.raise_for_status()  # throw an error if made a bad request
     if response:
         session.execute('TRUNCATE TABLE forecasts')  # clear the table
         response = response.json()
@@ -39,8 +38,7 @@ def scrape():
             session.add(Forecast(timestamp=timestamp, temperature=temperature,
                                  description=description, icon=icon))
         session.commit()
-    else:
-        print('Error')
 
 
-scrape()
+if __name__ == '__main__':
+    scrape()
