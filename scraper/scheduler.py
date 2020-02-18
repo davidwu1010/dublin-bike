bfc849b6f3818313210f48b86b527d2c5e81f84d
David Wu <wudawei01@gmail.com>
2020-02-18 11:34:38 +0000

    Implement scheudler.py

--- scraper/scheduler.py
+++ scraper/scheduler.py
@@ -3,7 +3,7 @@ import signal
 import sys
 import os
 from apscheduler.schedulers.blocking import BlockingScheduler
-from scraper import forecast_scraper
+from scraper import forecast_scraper, dublin_bike_data, current_weather_scraper
 
 
 def signal_handler(sig, frame):
@@ -18,9 +18,11 @@ sys.tracebacklimit = 0
 signal.signal(signal.SIGINT, signal_handler)  # trap SIGINT (CTRL+C)
 signal.signal(signal.SIGTERM, signal_handler)  # trap SIGTERM (kill)
 
-logging.basicConfig()
+logging.basicConfig(filename='scraper.log', filemode='a')
 logging.getLogger("apscheduler").setLevel(logging.DEBUG)
 
 scheduler = BlockingScheduler()
 scheduler.add_job(forecast_scraper.scrape, 'interval', hours=1)
+scheduler.add_job(dublin_bike_data.scrape, 'interval', minutes=5)
+scheduler.add_job(current_weather_scraper.scrape, 'interval', minutes=5)
 scheduler.start()
