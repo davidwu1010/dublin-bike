1862e1d664a4aba51337c83b4aada32d6e5a9be7
wenghsinping <ohmyhappy77@gmail.com>
2020-02-25 11:22:28 +0000

    fix get weather and datetime error

--- scraper/current_weather_scraper.py
+++ scraper/current_weather_scraper.py
@@ -8,6 +8,8 @@ from config import MySQL, APIKeys
 
 def scrape(craperDatetime):
 
+    print("craperDatetime", craperDatetime)
+
     engine = create_engine(f'mysql+pymysql://{MySQL.username}:{MySQL.password}@{MySQL.host}/{MySQL.database}')
     Base.metadata.create_all(engine)  # Create table
     Session = sessionmaker(bind=engine)
@@ -20,26 +22,25 @@ def scrape(craperDatetime):
         for station in stations:
 
             url = f'http://api.openweathermap.org/data/2.5/weather?lat={station.latitude}&lon={station.longitude}&appid={APIKeys.openweather_key}&units=metric'
-            # url = f'https://api.weatherbit.io/v2.0/current?lat={station.latitude}&lon={station.longitude}&key={APIKeys.weather_key}'
             response = requests.get(url)
             response.raise_for_status()
 
             if response:
 
                 data = response.json()
+
                 temp = data["main"]["temp"]
-                # datetimeStr = data["datetime"]
                 lon = data["coord"]["lon"]
                 lat = data["coord"]["lat"]
                 wind_spd = data["wind"]["speed"]
                 clouds = data["clouds"]["all"]
                 sunset = data["sys"]["sunset"]
-                weather = data["weather"]
+                weather = data["weather"][0]
                 icon = weather["icon"]
                 code = weather["id"]
                 description = weather["description"]
 
-                datetimeObj = datetime.datetime.strptime(craperDatetime, '%Y-%m-%d:%H')
+                datetimeObj = datetime.datetime.strptime(craperDatetime, '%Y-%m-%d %H:%M:%S')
                 weekday = datetimeObj.isoweekday()
 
                 # Only add new row to table of DB if the datetime(key) is not exist
@@ -53,7 +54,7 @@ def scrape(craperDatetime):
         print('Can not find stations')
 
 if __name__ == '__main__':
-    scrape()
+    scrape("2020-02-25 10")
 
 
 
